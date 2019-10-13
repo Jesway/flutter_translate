f9c114692a7006019c61f1018e38f747a88aaff1
Florin Bratan <florin@bratan.me>
2019-10-14 01:03:06 +0200

    Updated main pubspec

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 
 void main() async {
