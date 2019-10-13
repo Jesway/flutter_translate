079b4fa2d757189b27176e80db5a23af095763e8
Florin Bratan <florin@bratan.me>
2019-10-14 00:40:39 +0200

    translate, translatePlural and changeLanguage methods are now global

--- lib/global.dart
+++ lib/global.dart
@@ -1,6 +1,38 @@
+import 'package:flutter/cupertino.dart';
 import 'localization.dart';
+import 'localization_provider.dart';
+import 'localized_app.dart';
+
+Locale localeFromString(String code, {bool languageCodeOnly = false})
+{
+	if(code.contains('_'))
+	{
+		var parts = code.split('_');
+
+		return languageCodeOnly ? Locale(parts[0]) : Locale(parts[0], parts[1]);
+	}
+	else
+	{
+		return Locale(code);
+	}
+}
 
 String translate(String key, {Map<String, dynamic> args})
 {
 	return Localization.instance.translate(key, args: args);
+}
+
+String translatePlural(String key, num value, {Map<String, dynamic> args})
+{
+	return Localization.instance.plural(key, value, args: args);
+}
+
+void changeLanguage(BuildContext context, String localeCode)
+{
+	if(localeCode != null)
+	{
+		LocalizedApp.of(context).delegate.changeLanguage(localeFromString(localeCode));
+
+		LocalizationProvider.of(context).state.onLanguageChanged();
+	}
 }
