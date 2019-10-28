8e08237f62e66ef105ec02fe16eeb983d1b7e436
Florin Bratan <florin@bratan.me>
2019-10-28 21:46:30 +0100

    Updated documentation

--- packages/flutter_translate/example/lib/main.dart
+++ packages/flutter_translate/example/lib/main.dart
@@ -5,7 +5,9 @@ import 'package:flutter_translate/flutter_translate.dart';
 
 void main() async
 {
+  // Necessary since https://github.com/flutter/flutter/pull/38464 in order to access platform channels before runApp
   WidgetsFlutterBinding.ensureInitialized();
+
   var delegate = await LocalizationDelegate.create(
           fallbackLocale: 'en_US',
           supportedLocales: ['en_US', 'es', 'fa']);
