68de79e318677fd2a0ea29cf3aa177d1af39d760
Florin Bratan <florin@bratan.me>
2019-11-14 01:44:13 +0100

    Updated the travis configuration

---
+++ example_static_keys/lib/localization/keys.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'keys.dart';
+
+// **************************************************************************
+// Generator: FlutterTranslateGen
+// **************************************************************************
+
+class Keys {
+  static const String App_Bar_Title = 'app_bar.title';
+
+  static const String Button_Cancel = 'button.cancel';
+
+  static const String Button_Change_Language = 'button.change_language';
+
+  static const String Language_Name_En = 'language.name.en';
+
+  static const String Language_Name_Es = 'language.name.es';
+
+  static const String Language_Name_Fa = 'language.name.fa';
+
+  static const String Language_Selected_Message = 'language.selected_message';
+
+  static const String Language_Selection_Message = 'language.selection.message';
+
+  static const String Language_Selection_Title = 'language.selection.title';
+
+  static const String Plural_Demo = 'plural.demo';
+}
