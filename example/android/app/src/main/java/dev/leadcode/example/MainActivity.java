31365aac8d52e104e1df392fb658bc8c0ee9798e
felixmo <felixmo2008@hotmail.com>
2021-04-29 10:29:35 +0800

    Upgrade version of universal_io and collection, and update MainActivity

--- example/android/app/src/main/java/dev/leadcode/example/MainActivity.java
+++ example/android/app/src/main/java/dev/leadcode/example/MainActivity.java
@@ -2,12 +2,13 @@ package dev.leadcode.example;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
   }
 }
