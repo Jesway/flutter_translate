c46d6d8946d746620e6f7527eb616f05a4d2a42d
Florin Bratan <florin@bratan.me>
2023-11-20 13:28:28 +0100

    Implemented missing localization key service

---
+++ lib/src/services/missing_localization_key_service.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_translate/flutter_translate.dart';
+import 'package:flutter_translate/src/services/fallback_locale_service.dart';
+
+class MissingLocalizationKeyService
+{
+    FallbackLocaleService fallbackLocaleService;
+    FlutterTranslateOptions options;
+
+    MissingLocalizationKeyService(this.fallbackLocaleService, this.options);
+
+    Future initialize() async
+    {
+        if (options.missingKeyStrategy == MissingKeyStrategy.fallback)
+        {
+            await fallbackLocaleService.loadFallbackLocaleData();
+        }
+    }
+}
