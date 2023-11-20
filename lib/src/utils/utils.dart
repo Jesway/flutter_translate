b685c35a305350bceb415124ba77263f050162db
Florin Bratan <florin@bratan.me>
2023-11-20 13:23:21 +0100

    Refactored utils

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_translate/flutter_translate.dart';
-
-typedef LocaleChangedCallback = Future Function(Locale locale);
 
 Locale localeFromString(String code, {bool languageCodeOnly = false})
 {
@@ -20,27 +17,4 @@ Locale localeFromString(String code, {bool languageCodeOnly = false})
 String localeToString(Locale locale)
 {
 	return locale.countryCode != null ? '${locale.languageCode}_${locale.countryCode}' : locale.languageCode;
-}
-
-/// Translate the selected key into the currently selected locale
-String translate(String key, {Map<String, dynamic>? args})
-{
-	return Localization.instance.translate(key, args: args);
-}
-
-/// Translate the selected key into the currently selected locale using pluralization
-String translatePlural(String key, num value, {Map<String, dynamic>? args})
-{
-	return Localization.instance.plural(key, value, args: args);
-}
-
-/// Change the currently selected locale
-Future changeLocale(BuildContext context, String? localeCode) async
-{
-	if (localeCode != null)
-	{
-		await LocalizedApp.of(context).delegate.changeLocale(localeFromString(localeCode));
-
-		LocalizationProvider.of(context).state.onLocaleChanged();
-	}
 }
