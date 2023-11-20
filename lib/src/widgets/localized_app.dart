250b95009ab34da5108989be366eae1db7c115b5
Florin Bratan <florin@bratan.me>
2023-11-20 13:23:13 +0100

    Updated localized app widgets

--- lib/src/widgets/localized_app.dart
+++ lib/src/widgets/localized_app.dart
@@ -1,15 +1,69 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_translate/flutter_translate.dart';
+import 'package:flutter_translate/src/delegates/flutter_translate_delegate.dart';
 
-class LocalizedApp extends StatefulWidget
+class LocalizedApp extends StatefulWidget 
 {
     final Widget child;
 
-    final LocalizationDelegate delegate;
+    const LocalizedApp(this.child, {super.key});
 
-    LocalizedApp(this.delegate, this.child);
+    static LocalizedAppState of(BuildContext context) 
+    {
+        final inheritedWidget = context.dependOnInheritedWidgetOfExactType<LocalizedAppInherited>();
 
+        assert(inheritedWidget != null, 'No LocalizedApp found in context');
+        
+        return inheritedWidget!.data;
+    }
+
+    @override
     LocalizedAppState createState() => LocalizedAppState();
+}
 
-    static LocalizedApp of(BuildContext context) => context.findAncestorWidgetOfExactType<LocalizedApp>()!;
+class LocalizedAppState extends State<LocalizedApp> 
+{
+    final delegate = FlutterTranslateDelegate();
+
+    List<LocalizationsDelegate> get delegates => [
+        delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+    ];
+
+    void refresh()
+    {
+        setState(() {});
+    }
+
+    @override
+    Widget build(BuildContext context) 
+    {
+        return LocalizedAppInherited(
+            data: this,
+            currentLocale: FlutterTranslate.instance.currentLocale,
+            child: widget.child,
+        );
+    }
+}
+
+class LocalizedAppInherited extends InheritedWidget 
+{
+    final LocalizedAppState data;
+    final Locale currentLocale;
+
+    const LocalizedAppInherited({
+        super.key,
+        required this.data,
+        required this.currentLocale,
+        required super.child,
+    });
+
+    @override
+    bool updateShouldNotify(LocalizedAppInherited oldWidget) 
+    {
+        return oldWidget.currentLocale != currentLocale;
+    }
 }
