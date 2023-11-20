0ea864f8f3a8afec3445e51dc6d189b0dd70e3de
Florin Bratan <florin@bratan.me>
2023-11-20 13:26:26 +0100

    Added build context extensions

---
+++ lib/src/extensions/build_context_extensions.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+
+extension BuildContextExtensions on BuildContext 
+{
+    Locale get locale => FlutterTranslate.instance.currentLocale;
+
+    List<Locale> get supportedLocales => FlutterTranslate.instance.supportedLocales;
+
+    List<LocalizationsDelegate> get localizationDelegates => LocalizedApp.of(this).delegates;   
+}
